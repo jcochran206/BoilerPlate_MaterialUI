b29a9d89c9375f4fef8de93df45d9d2bffd28c3d
jcochran206 <jonathan.o.cochran@gmail.com>
2023-06-20 15:15:44 -0700

    added appbar and toolbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Typography, Button, AppBar, Card, CardActions, CardContent,CardMedia, CssBaseline, Grid, Toolbar, Container} from '@mui/material';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon />
+          <Typography variant='h6'>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth='sm'>
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              this is a photo album this will be a long sentence 
+            </Typography>
+            <div>
+              <Grid container spacing={3} justify="center">
+                <Grid item>
+                  <Button variant='contained' color='primary'> 
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'> 
+                    Secondary button
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 }
 
